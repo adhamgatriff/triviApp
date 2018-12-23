de0248763f861b49c591302b47ae3edf198333d2
adhamgatriff <adhamgatriff@gmail.com>
2018-12-23 16:26:58 -0400

    bringing style and logic to questionScreen

--- components/routes/index.js
+++ components/routes/index.js
@@ -2,14 +2,18 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import Home from '../views/Home';
 import Menu from '../views/Menu';
 
+// remove
+import GenericComponent from '../views/genericComponent';
+
 export default createAppContainer(
   createStackNavigator({
     Home: { screen: Home },
     Menu: { screen: Menu },
+    GenericComponent: { screen: GenericComponent },
   },
   {
     headerMode: 'none',
-    initialRouteName: 'Home',
+    initialRouteName: 'GenericComponent',
     navigationOptions: {
       gesturesEnabled: false,
     },
